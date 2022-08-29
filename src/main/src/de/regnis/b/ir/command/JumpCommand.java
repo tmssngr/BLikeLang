34ee4cd5a002703742381d198701d8a2869ef3b3
Thomas Singer <thomas.singer@syntevo.com>
2022-08-29 19:44:08 +0200

    CommandFactory: CmpJump -> Cmp + Jumps

--- src/main/src/de/regnis/b/ir/command/JumpCommand.java
+++ src/main/src/de/regnis/b/ir/command/JumpCommand.java
@@ -1,16 +1,26 @@
 package de.regnis.b.ir.command;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * @author Thomas Singer
  */
-public record JumpCommand(@NotNull String label) implements Command {
+public record JumpCommand(@Nullable JumpCondition condition, @NotNull String label) implements Command {
+
+	// Setup ==================================================================
+
+	public JumpCommand(@NotNull String label) {
+		this(null, label);
+	}
 
 	// Implemented ============================================================
 
 	@Override
 	public String toString() {
+		if (condition != null) {
+			return "jp " + condition + ", " + label;
+		}
 		return "jp " + label;
 	}
 }
