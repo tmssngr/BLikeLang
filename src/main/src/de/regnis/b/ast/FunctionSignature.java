2f380ed54cfaeb212d43d65ea56f92cfb54dccf8
Thomas Singer <tmssngr@users.noreply.github.com>
2022-08-30 21:32:06 +0200

    FunctionSignature

---
+++ src/main/src/de/regnis/b/ast/FunctionSignature.java
@@ -0,0 +1,10 @@
+package de.regnis.b.ast;
+
+import de.regnis.b.type.Type;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author Thomas Singer
+ */
+public record FunctionSignature(@NotNull Type returnType, int parameterCount) {
+}
