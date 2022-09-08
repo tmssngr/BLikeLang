5398de3e4c488b6d6e0771b54afcd6c144a535a3
Thomas Singer <tmssngr@users.noreply.github.com>
2022-09-08 17:32:43 +0200

    Compiler

---
+++ src/main/src-test/de/regnis/b/CompilerTest.java
@@ -0,0 +1,417 @@
+package de.regnis.b;
+
+import de.regnis.b.out.StringOutput;
+import de.regnis.b.out.StringStringOutput;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Thomas Singer
+ */
+public class CompilerTest {
+
+	// Accessing ==============================================================
+
+	@Test
+	public void test1() {
+		test("""
+				     void print(int v) {
+				     }
+				     int max(int a, int b) {
+				       if (a > b) {
+				         return a;
+				       }
+				       return b;
+				     }
+				     void main() {
+				       print(max(1, 2));
+				     }""",
+		     Messages.warningUnusedParameter(1, 15, "v") + "\n",
+		     """
+				     main:
+				         push r4
+				         push r5
+				         ld r0, #%00
+				         ld r1, #%01
+				         push r0
+				         push r1
+				         ld r0, #%00
+				         ld r1, #%02
+				         push r0
+				         push r1
+				         call max
+				         pop r1
+				         pop r0
+				         pop r1
+				         pop r0
+				         ld r4, r0
+				         ld r5, r1
+				         push r0
+				         push r1
+				         call print
+				         pop r1
+				         pop r0
+				         pop r5
+				         pop r4
+				         ret
+
+				     print:
+				         ret
+
+				     max:
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%04
+				         adc r14, #%00
+				         lde r2, @rr14
+				         incw r14
+				         lde r3, @rr14
+				         cp r0, r2
+				         .jp gt, max_then_1
+				         .jp nz, max_exit
+				         cp r1, r3
+				         .jp ule, max_exit
+				     max_then_1:
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%04
+				         adc r14, #%00
+				         lde @rr14, r0
+				         incw r14
+				         lde @rr14, r1
+				     max_exit:
+				         ret
+				     """);
+	}
+
+	@Test
+	public void test2() {
+		test("""
+				     void main() {
+				       int address = 0x4000
+				       int count = 8 * 8
+				       while (count > 0) {
+				         int value = getMem(address)
+				         setMem(address, value ^ 0xFF)
+				         address += 1
+				         count -= 1
+				       }
+				     }
+				     """,
+		     "",
+		     """
+				     main:
+				         push r4
+				         push r5
+				         push r6
+				         push r7
+				         push r8
+				         push r9
+				         ld r0, #%40
+				         ld r1, #%00
+				         ld r4, r0
+				         ld r5, r1
+				         ld r0, #%00
+				         ld r1, #%40
+				         ld r6, r0
+				         ld r7, r1
+				     main_while_1:
+				         ld r0, r6
+				         ld r1, r7
+				         cp r0, #%00
+				         .jp gt, main_do_1
+				         .jp nz, main_exit
+				         cp r1, #%00
+				         .jp ule, main_exit
+				     main_do_1:
+				         ld r0, r4
+				         ld r1, r5
+				         lde r1, @rr0
+				         ld r0, #%00
+				         ld r8, r0
+				         ld r9, r1
+				         com r1
+				         ld r8, r0
+				         ld r9, r1
+				         ld r0, r4
+				         ld r1, r5
+				         ld r2, r8
+				         ld r3, r9
+				         lde @rr0, r3
+				         ld r0, r4
+				         ld r1, r5
+				         incw r0
+				         ld r4, r0
+				         ld r5, r1
+				         ld r0, r6
+				         ld r1, r7
+				         decw r0
+				         ld r6, r0
+				         ld r7, r1
+				         .jp main_while_1
+				     main_exit:
+				         pop r9
+				         pop r8
+				         pop r7
+				         pop r6
+				         pop r5
+				         pop r4
+				         ret
+				     """);
+	}
+
+	@Test
+	public void test3() {
+		test("""
+				     int getInput() {
+				       return 192
+				     }
+
+				     void printHex4(int i) {
+				       i = i & 15
+				       int chr = 0
+				       if (i < 10) {
+				         chr = i + '0'
+				       }
+				       else {
+				         chr = i - 10 + 'A'
+				       }
+				       printChar(chr);
+				     }
+
+				     void printHex8(int i) {
+				       printHex4(i >> 4)
+				       printHex4(i);
+				     }
+
+				     void printHex16(int i) {
+				       printHex8(i >> 8);
+				       printHex8(i);
+				     }
+
+				     void main() {
+				       int input = getInput()
+				       printHex16(input);
+				     }""",
+		     "",
+		     """
+				     main:
+				         push r4
+				         push r5
+				         push r0
+				         push r1
+				         call getInput
+				         pop r1
+				         pop r0
+				         ld r4, r0
+				         ld r5, r1
+				         push r0
+				         push r1
+				         call printHex16
+				         pop r1
+				         pop r0
+				         pop r5
+				         pop r4
+				         ret
+
+				     getInput:
+				         ld r0, #%00
+				         ld r1, #%C0
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde @rr14, r0
+				         incw r14
+				         lde @rr14, r1
+				         ret
+
+				     printHex4:
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         and r1, #%0F
+				         ld r0, #%00
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde @rr14, r0
+				         incw r14
+				         lde @rr14, r1
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         cp r0, #%00
+				         .jp lt, printHex4_then_1
+				         .jp nz, printHex4_else_1
+				         cp r1, #%0A
+				         .jp uge, printHex4_else_1
+				     printHex4_then_1:
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         add r1, #%30
+				         adc r0, #%00
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde @rr14, r0
+				         incw r14
+				         lde @rr14, r1
+				         .jp printHex4_after_if_1
+				     printHex4_else_1:
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         add r1, #%37
+				         adc r0, #%00
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde @rr14, r0
+				         incw r14
+				         lde @rr14, r1
+				     printHex4_after_if_1:
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%02
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         ld %15, r1
+				         call %0818
+				         ret
+
+				     printHex8:
+				         push r4
+				         push r5
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%04
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         ld r4, r0
+				         ld r5, r1
+				         ccf
+				         rrc r0
+				         rrc r1
+				         ccf
+				         rrc r0
+				         rrc r1
+				         ccf
+				         rrc r0
+				         rrc r1
+				         ccf
+				         rrc r0
+				         rrc r1
+				         ld r4, r0
+				         ld r5, r1
+				         push r0
+				         push r1
+				         call printHex4
+				         pop r1
+				         pop r0
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%04
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         push r0
+				         push r1
+				         call printHex4
+				         pop r1
+				         pop r0
+				         pop r5
+				         pop r4
+				         ret
+
+				     printHex16:
+				         push r4
+				         push r5
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%04
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         ld r4, r0
+				         ld r5, r1
+				         ld r1, r0
+				         ld r0, #%00
+				         ld r4, r0
+				         ld r5, r1
+				         push r0
+				         push r1
+				         call printHex8
+				         pop r1
+				         pop r0
+				         ld r14, %FE
+				         ld r15, %FF
+				         add r15, #%04
+				         adc r14, #%00
+				         lde r0, @rr14
+				         incw r14
+				         lde r1, @rr14
+				         push r0
+				         push r1
+				         call printHex8
+				         pop r1
+				         pop r0
+				         pop r5
+				         pop r4
+				         ret
+				     """);
+	}
+
+	// Utils ==================================================================
+
+	private void test(String input, String expectedWarnings, String expectedOutput) {
+		final StringOutput warnings = new StringStringOutput();
+		final Compiler compiler = new Compiler(warnings);
+		final StringOutput asmOutput = new StringStringOutput();
+
+		compiler.compile(input, asmOutput);
+
+		Assert.assertEquals(expectedWarnings, warnings.toString());
+		Assert.assertEquals(expectedOutput, asmOutput.toString());
+	}
+}
---
+++ src/main/src/de/regnis/b/Compiler.java
@@ -0,0 +1,255 @@
+package de.regnis.b;
+
+import de.regnis.b.ast.*;
+import de.regnis.b.ast.transformation.DetermineTypesTransformation;
+import de.regnis.b.ast.transformation.RemoveUnusedFunctionsTransformation;
+import de.regnis.b.ast.transformation.ReplaceModifyAssignmentWithBinaryExpressionTransformation;
+import de.regnis.b.ir.*;
+import de.regnis.b.ir.command.*;
+import de.regnis.b.out.PathStringOutput;
+import de.regnis.b.out.StringOutput;
+import de.regnis.b.out.StringStringOutput;
+import de.regnis.b.type.BasicTypes;
+import de.regnis.b.type.Type;
+import de.regnis.utils.Utils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+
+import static de.regnis.utils.Utils.notNull;
+
+/**
+ * @author Thomas Singer
+ */
+@SuppressWarnings("UseOfSystemOutOrSystemErr")
+public final class Compiler {
+
+	// Static =================================================================
+
+	public static boolean DEBUG;
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 1) {
+			System.out.println("Missing input file");
+			return;
+		}
+
+		final Path inputFile = Paths.get(args[0]);
+		final String input = Files.readString(inputFile);
+
+		final var compiler = new Compiler(new StringOutput() {
+			@Override
+			public void print(@NotNull String s) {
+				System.out.print(s);
+			}
+
+			@Override
+			public void println() {
+				System.out.println();
+			}
+		});
+		try (final PathStringOutput output = new PathStringOutput(inputFile.resolveSibling("output.asm"))) {
+			output.print(ControlFlowGraphPrinter.INDENTATION);
+			output.print(".org %8000");
+			output.println();
+
+			output.print(ControlFlowGraphPrinter.INDENTATION);
+			// set RP
+			output.print("ld %FD, #%20");
+			output.println();
+
+			compiler.compile(input, output);
+		}
+	}
+
+	// Fields =================================================================
+
+	private final StringOutput output;
+	private final BuiltInFunctions builtInFunctions;
+
+	// Setup ==================================================================
+
+	public Compiler(@NotNull StringOutput output) {
+		this.output = output;
+
+		builtInFunctions = new BuiltInFunctions();
+		builtInFunctions.add(BasicTypes.VOID, "printChar", 1, new BuiltInFunctions.FunctionCommandFactory() {
+			@Override
+			public void handleCall(@NotNull List<Expression> parameters, @Nullable String assignReturnToVar, @NotNull BuiltInFunctions.CommandFactory factory) {
+				Utils.assertTrue(parameters.size() == 1);
+				Utils.assertTrue(assignReturnToVar == null);
+
+				factory.loadToRegister(parameters.get(0), 0);
+				factory.addCommand(new Ld(0x15, CommandFactory.workingRegister(1)));
+				factory.addCommand(new CallCommand("%0818"));
+			}
+		});
+		builtInFunctions.add(BasicTypes.INT16, "getMem", 1, new BuiltInFunctions.FunctionCommandFactory() {
+			@Override
+			public void handleCall(@NotNull List<Expression> parameters, @Nullable String assignReturnToVar, @NotNull BuiltInFunctions.CommandFactory factory) {
+				Utils.assertTrue(parameters.size() == 1);
+				Utils.assertTrue(assignReturnToVar != null);
+
+				factory.loadToRegister(parameters.get(0), 0);
+				factory.addCommand(new LdFromMem(1, 0));
+				factory.addCommand(new LdLiteral(CommandFactory.workingRegister(0), 0));
+				factory.saveToVar(assignReturnToVar, 0);
+			}
+		});
+		builtInFunctions.add(BasicTypes.VOID, "setMem", 2, new BuiltInFunctions.FunctionCommandFactory() {
+			@Override
+			public void handleCall(@NotNull List<Expression> parameters, @Nullable String assignReturnToVar, @NotNull BuiltInFunctions.CommandFactory factory) {
+				Utils.assertTrue(parameters.size() == 2);
+				Utils.assertTrue(assignReturnToVar == null);
+
+				factory.loadToRegister(parameters.get(0), 0);
+				factory.loadToRegister(parameters.get(1), 2);
+				factory.addCommand(new LdToMem(0, 3));
+			}
+		});
+	}
+
+	// Accessing ==============================================================
+
+	public void compile(@NotNull String input, @NotNull StringOutput asmOutput) {
+		DeclarationList root = AstFactory.parseString(input);
+		root = DetermineTypesTransformation.transform(root, builtInFunctions, output);
+		root = RemoveUnusedFunctionsTransformation.transform(root, builtInFunctions, output);
+		root = ReplaceModifyAssignmentWithBinaryExpressionTransformation.transform(root);
+
+		final Function<String, Type> functionNameToReturnType = new FunctionNameToReturnType(root);
+
+		final List<Declaration> declarations = new ArrayList<>(root.getDeclarations());
+		final FuncDeclaration mainFunction = notNull(root.getFunction("main"));
+		declarations.remove(mainFunction);
+		declarations.add(0, mainFunction);
+
+		boolean addSeparator = false;
+		for (Declaration declaration : declarations) {
+			if (addSeparator) {
+				asmOutput.println();
+			}
+			addSeparator = true;
+			declaration.visit(new DeclarationVisitor<>() {
+				@Override
+				public Object visitFunctionDeclaration(FuncDeclaration node) {
+					processFunction(node, asmOutput, functionNameToReturnType);
+					return node;
+				}
+			});
+		}
+	}
+
+	// Utils ==================================================================
+
+	private void processFunction(FuncDeclaration declaration, StringOutput output, Function<String, Type> functionNameToReturnType) {
+		final ControlFlowGraph cfg = new ControlFlowGraph(declaration);
+
+		CfgSimplifyExpressions.transform(cfg);
+		CfgParameterToSimpleExpression.transform(cfg);
+		if (DEBUG) {
+			System.out.println("before ssa");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		StaticSingleAssignmentFactory.transform(cfg);
+		if (DEBUG) {
+			System.out.println("ssa");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		simplify(cfg);
+		if (DEBUG) {
+			System.out.println("ssa optimized");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		SplitExpressionsTransformation.transform(cfg);
+		if (DEBUG) {
+			System.out.println("ssa split expressions");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		SsaToModifyAssignments.transform(cfg);
+		if (DEBUG) {
+			System.out.println("ssa modify");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		SsaRemovePhiFunctions.transform(cfg);
+		if (DEBUG) {
+			System.out.println("removed phi functions");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		final RegisterAllocation.Result registers = RegisterAllocation.run(cfg);
+
+		CfgReuseVarsTransformation.transform(cfg, registers);
+		if (DEBUG) {
+			System.out.println("reuse vars");
+			System.out.println(ControlFlowGraphPrinter.print(cfg, new StringStringOutput()));
+		}
+
+		final StackPositionProvider stackPositionProvider = new StackPositionProviderImpl(registers, 5, 4);
+
+		CommandList commandList = new CommandList();
+
+		final CommandFactory commandFactory = new CommandFactory(stackPositionProvider, functionNameToReturnType, builtInFunctions, commandList);
+		commandFactory.addPrelude(declaration, registers);
+
+		final List<AbstractBlock> blocks = cfg.getLinearizedBlocks();
+		for (AbstractBlock block : blocks) {
+			commandFactory.add(block);
+		}
+
+		if (DEBUG) {
+			System.out.println("command line");
+			commandList.print(StringOutput.out);
+		}
+
+		commandList = CommandListOptimizations.optimize(commandList);
+		commandList.print(output);
+	}
+
+	private void simplify(ControlFlowGraph cfg) {
+		while (true) {
+			boolean changed = false;
+			final Set<String> unusedVariables = SsaUnusedVarDetector.detectUnusedVariables(cfg);
+			if (unusedVariables.size() > 0) {
+				changed = true;
+				SsaSearchAndReplace.remove(cfg, unusedVariables);
+			}
+
+			if (SsaConstantDetection.transform(cfg)) {
+				changed = true;
+			}
+
+			if (!changed) {
+				break;
+			}
+		}
+	}
+
+	// Inner Classes ==========================================================
+
+	private record FunctionNameToReturnType(DeclarationList root) implements Function<String, Type> {
+			private FunctionNameToReturnType(@NotNull DeclarationList root) {
+				this.root = root;
+			}
+
+			@Nullable
+			@Override
+			public Type apply(String functionName) {
+				final FuncDeclaration function = root.getFunction(functionName);
+				return function != null ? function.type() : null;
+			}
+		}
+}
