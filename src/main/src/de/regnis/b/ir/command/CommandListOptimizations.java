dc897356b4cf6ab1d4089a1b86bad17accd58508
Thomas Singer <thomas.singer@syntevo.com>
2022-09-08 13:17:08 +0200

    CommandListOptimizations: optimize useless commands

--- src/main/src/de/regnis/b/ir/command/CommandListOptimizations.java
+++ src/main/src/de/regnis/b/ir/command/CommandListOptimizations.java
@@ -2,10 +2,8 @@ package de.regnis.b.ir.command;
 
 import de.regnis.utils.Utils;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
-import java.util.function.Function;
 
 /**
  * @author Thomas Singer
@@ -25,6 +23,11 @@ public final class CommandListOptimizations {
 			List<Command> processedCommands = removeUselessJumpsToNextCommand(commands);
 			processedCommands = replaceLabels(processedCommands);
 			processedCommands = removeUnusedLabels(processedCommands);
+			processedCommands = filterAddSub(processedCommands);
+			processedCommands = filterAndOrXor(processedCommands);
+			processedCommands = filterLd3(processedCommands);
+			processedCommands = filterLd2(processedCommands);
+
 			if (processedCommands.equals(commands)) {
 				final CommandList commandList = new CommandList();
 				processedCommands.forEach(commandList::add);
@@ -38,43 +41,39 @@ public final class CommandListOptimizations {
 	// Utils ==================================================================
 
 	private static List<Command> removeUselessJumpsToNextCommand(List<Command> commands) {
-		final List<Command> newCommands = new ArrayList<>();
-
-		for (int i = 0; i < commands.size(); i++) {
-			final Command command = commands.get(i);
-			if (i < commands.size() - 1
-					&& command instanceof JumpCommand jumpCommand
-					&& commands.get(i + 1) instanceof Label label
-					&& label.name().equals(jumpCommand.label())) {
-				continue;
+		return filterCommands(commands, new DualCommandVisitor() {
+			@Override
+			public void visit(@NotNull Command command1, @NotNull Command command0, @NotNull Stack stack) {
+				if (command1 instanceof JumpCommand jumpCommand
+						&& command0 instanceof Label label
+						&& label.name().equals(jumpCommand.label())) {
+					stack.drop();
+					stack.drop();
+					stack.add(command0);
+				}
 			}
-
-			newCommands.add(command);
-		}
-
-		return newCommands;
+		});
 	}
 
 	private static List<Command> replaceLabels(List<Command> commands) {
 		final Map<String, String> labelToLabel = findLabelsPointingToSameLocation(commands);
 
-		return filterCommands(commands, new Function<>() {
-			@Nullable
+		return filterCommands(commands, new SingleCommandVisitor() {
 			@Override
-			public Command apply(Command command) {
+			public void visit(@NotNull Command command, @NotNull Stack stack) {
 				if (command instanceof JumpCommand jumpCommand) {
 					final String newLabel = labelToLabel.get(jumpCommand.label());
 					if (newLabel != null) {
-						return new JumpCommand(jumpCommand.condition(), newLabel);
+						stack.drop();
+						stack.add(new JumpCommand(jumpCommand.condition(), newLabel));
 					}
+					return;
 				}
 
 				if (command instanceof Label label
 						&& labelToLabel.containsKey(label.name())) {
-					return null;
+					stack.drop();
 				}
-
-				return command;
 			}
 		});
 	}
@@ -108,16 +107,13 @@ public final class CommandListOptimizations {
 	private static List<Command> removeUnusedLabels(List<Command> commands) {
 		final Set<String> usedLabels = determineUsedLabels(commands);
 
-		return filterCommands(commands, new Function<>() {
-			@Nullable
+		return filterCommands(commands, new SingleCommandVisitor() {
 			@Override
-			public Command apply(Command command) {
+			public void visit(@NotNull Command command, @NotNull Stack stack) {
 				if (command instanceof Label label
 						&& !usedLabels.contains(label.name())) {
-					return null;
+					stack.drop();
 				}
-
-				return command;
 			}
 		});
 	}
@@ -132,17 +128,234 @@ public final class CommandListOptimizations {
 		return usedLabels;
 	}
 
-	private static List<Command> filterCommands(List<Command> commands, Function<Command, Command> function) {
-		final List<Command> newCommands = new ArrayList<>();
+	@NotNull
+	private static List<Command> filterAddSub(List<Command> commands) {
+		return filterCommands(commands, new DualCommandVisitor() {
+			@Override
+			public void visit(@NotNull Command command1, @NotNull Command command0, @NotNull Stack stack) {
+				if (command1 instanceof ArithmeticC ac1
+						&& command0 instanceof ArithmeticC ac0
+						&& ac1.register() == ac0.register() + 1
+						&& ac1.literal() == 1
+						&& ac0.literal() == 0) {
+					if (ac1.op() == ArithmeticOp.add
+							&& ac0.op() == ArithmeticOp.adc) {
+						stack.drop();
+						stack.drop();
+						stack.add(new RegisterCommand(RegisterCommand.Op.incw, ac0.register()));
+						return;
+					}
+					if (ac1.op() == ArithmeticOp.sub
+							&& ac0.op() == ArithmeticOp.sbc) {
+						stack.drop();
+						stack.drop();
+						stack.add(new RegisterCommand(RegisterCommand.Op.decw, ac0.register()));
+					}
+				}
+			}
+		});
+	}
 
-		// start at second command, because the first (method label) must be kept
-		for (int i = 0; i < commands.size(); i++) {
-			final Command command = commands.get(i);
-			final Command newCommand = i > 0 ? function.apply(command) : command;
-			if (newCommand != null) {
-				newCommands.add(newCommand);
+	@NotNull
+	private static List<Command> filterAndOrXor(List<Command> commands) {
+		return filterCommands(commands, new SingleCommandVisitor() {
+			@Override
+			public void visit(@NotNull Command command, @NotNull Stack stack) {
+				if (command instanceof ArithmeticC ac) {
+					if (ac.op() == ArithmeticOp.and) {
+						if (ac.literal() == 0xFF) {
+							stack.drop();
+							return;
+						}
+
+						if (ac.literal() == 0) {
+							stack.drop();
+							stack.add(new LdLiteral(ac.register(), 0));
+							return;
+						}
+					}
+					if (ac.op() == ArithmeticOp.or) {
+						if (ac.literal() == 0) {
+							stack.drop();
+							return;
+						}
+
+						if (ac.literal() == 0xFF) {
+							stack.drop();
+							stack.add(new LdLiteral(ac.register(), 0xFF));
+							return;
+						}
+					}
+					if (ac.op() == ArithmeticOp.xor) {
+						if (ac.literal() == 0) {
+							stack.drop();
+							return;
+						}
+
+						if (ac.literal() == 0xFF) {
+							stack.drop();
+							stack.add(new RegisterCommand(RegisterCommand.Op.com, ac.register()));
+							return;
+						}
+					}
+				}
+			}
+		});
+	}
+
+	@NotNull
+	private static List<Command> filterLd3(List<Command> commands) {
+		return filterCommands(commands, new TripleCommandVisitor() {
+			@Override
+			public void visit(@NotNull Command command2, @NotNull Command command1, @NotNull Command command0, @NotNull Stack stack) {
+				// Example:
+				// ld r8, r0 (ld2)
+				// ld r9, r1
+				// ld r0, r8 (ld0)
+				// ld r1, r9
+				if (command2 instanceof Ld ld2
+						&& command0 instanceof Ld ld0
+						&& ld2.sourceRegister() == ld0.targetRegister()
+						&& ld0.sourceRegister() == ld2.targetRegister()) {
+					if (!doesNotChange(command1, ld0.sourceRegister())) {
+						return;
+					}
+					stack.drop();
+				}
+			}
+		});
+	}
+
+	private static boolean doesNotChange(Command command, int register) {
+		if (command instanceof CallCommand
+				|| command instanceof JumpCommand
+				|| command instanceof Label) {
+			return false;
+		}
+		if (command instanceof Arithmetic a) {
+			return a.srcRegister() != register;
+		}
+		if (command instanceof ArithmeticC a) {
+			return a.register() != register;
+		}
+		if (command instanceof Ld ld) {
+			return ld.targetRegister() != register;
+		}
+		if (command instanceof LdLiteral ld) {
+			return ld.register() != register;
+		}
+		if (command instanceof LdFromMem lfm) {
+			return lfm.targetRegister() != register;
+		}
+		if (command instanceof RegisterCommand r) {
+			if (r.op() == RegisterCommand.Op.push) {
+				return true;
+			}
+			//noinspection SimplifiableIfStatement
+			if ((r.op() == RegisterCommand.Op.decw
+					|| r.op() == RegisterCommand.Op.incw)
+					&& (r.register() & 0xFE) == (register & 0xFE)) {
+				return true;
+			}
+			return r.register() != register;
+		}
+		throw new IllegalStateException();
+	}
+
+	@NotNull
+	private static List<Command> filterLd2(List<Command> commands) {
+		return filterCommands(commands, new DualCommandVisitor() {
+			@Override
+			public void visit(@NotNull Command command1, @NotNull Command command0, @NotNull Stack stack) {
+				// Example:
+				// ld r8, r0 (ld1)
+				// ld r0, r8 (ld0)
+				// ld r1, r9
+				if (command1 instanceof Ld ld1
+						&& command0 instanceof Ld ld0
+						&& ld1.sourceRegister() == ld0.targetRegister()
+						&& ld0.sourceRegister() == ld1.targetRegister()) {
+					stack.drop();
+				}
+			}
+		});
+	}
+
+	private static List<Command> filterCommands(List<Command> commands, SingleCommandVisitor filter) {
+		final List<Command> newCommands = new ArrayList<>();
+		final Stack stack = new StackImpl(newCommands);
+		for (Command command : commands) {
+			stack.add(command);
+			if (newCommands.size() > 1) {
+				filter.visit(command, stack);
 			}
 		}
+
 		return newCommands;
+	}
+
+	private static List<Command> filterCommands(List<Command> commands, DualCommandVisitor filter) {
+		final List<Command> newCommands = new ArrayList<>();
+		final Stack stack = new StackImpl(newCommands);
+		for (Command command : commands) {
+			stack.add(command);
+			if (newCommands.size() > 1) {
+				final Command prevCommand = newCommands.get(newCommands.size() - 2);
+				filter.visit(prevCommand, command, stack);
+			}
+		}
+
+		return newCommands;
+	}
+
+	private static List<Command> filterCommands(List<Command> commands, TripleCommandVisitor filter) {
+		final List<Command> newCommands = new ArrayList<>();
+		final Stack stack = new StackImpl(newCommands);
+		for (Command command : commands) {
+			stack.add(command);
+			if (newCommands.size() > 2) {
+				final Command prevCommand2 = newCommands.get(newCommands.size() - 3);
+				final Command prevCommand = newCommands.get(newCommands.size() - 2);
+				filter.visit(prevCommand2, prevCommand, command, stack);
+			}
+		}
+
+		return newCommands;
+	}
+
+	// Inner Classes ==========================================================
+
+	private interface SingleCommandVisitor {
+
+		void visit(@NotNull Command command, @NotNull Stack stack);
+	}
+
+	private interface DualCommandVisitor {
+
+		void visit(@NotNull Command command1, @NotNull Command command0, @NotNull Stack stack);
+	}
+
+	private interface TripleCommandVisitor {
+
+		void visit(@NotNull Command command2, @NotNull Command command1, @NotNull Command command0, @NotNull Stack stack);
+	}
+
+	private interface Stack {
+		void add(@NotNull Command command);
+
+		void drop();
+	}
+
+	private record StackImpl(List<Command> commands) implements Stack {
+
+		@Override
+		public void add(@NotNull Command command) {
+			commands.add(command);
+		}
+
+		@Override
+		public void drop() {
+			commands.remove(commands.size() - 1);
+		}
 	}
 }
