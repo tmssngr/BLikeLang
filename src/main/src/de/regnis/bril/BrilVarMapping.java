e4f47eaa6874369085d1f8f19a66c1e7e93d3b72
Thomas Singer <tmssngr@users.noreply.github.com>
2023-07-17 20:24:45 +0200

    BrilVarMapping cleanup

--- src/main/src/de/regnis/bril/BrilVarMapping.java
+++ src/main/src/de/regnis/bril/BrilVarMapping.java
@@ -1,6 +1,5 @@
 package de.regnis.bril;
 
-import de.regnis.b.ir.RegisterColoring;
 import de.regnis.utils.Utils;
 
 import java.util.*;
@@ -76,34 +75,22 @@ final class BrilVarMapping {
 		asm.freeSpace(localBytes);
 	}
 
-	public void callArgument(String arg, int argIndex, BrilAsm asm) {
-/*
-		if (argIndex == 0) {
-			iloadTo(arg, ARG0_REGISTER, asm);
-		}
-		else if (argIndex == 1) {
-			iloadTo(arg, ARG1_REGISTER, asm);
+	public void ireturnValueFromVar(String var, BrilAsm asm) {
+		final BrilVars.VarLocation location = getLocation(var);
+		if (location.isRegister()) {
+			final int register = location.reg();
+			if (register != ARG0_REGISTER) {
+				asm.iload(ARG0_REGISTER, register);
+			}
 		}
 		else {
-			final int register = iload(arg, A_REGISTER, asm);
-			asm.ipush(register);
+			asm.iloadFromStack(ARG0_REGISTER, FP_REGISTER, location.offset());
 		}
-*/
-	}
-
-	public void ireturnValueFromVar(String var, BrilAsm asm) {
-		iloadTo(var, ARG0_REGISTER, asm);
 	}
 
 	public void loadConstant(String dest, int value, BrilAsm asm) {
-		final BrilVars.VarLocation location = getLocation(dest);
-		if (location.isRegister()) {
-			asm.iconst(location.reg(), value);
-		}
-		else {
-			asm.iconst(0, value);
-			asm.istoreToStack(0, FP_REGISTER, location.offset());
-		}
+		final int register = getRegisterLocation(dest);
+		asm.iconst(register, value);
 	}
 
 	public void i_binaryOperator(String dest, String var1, String var2, BrilAsm asm, I_BinaryRegisterOperator operator) {
@@ -112,18 +99,6 @@ final class BrilVarMapping {
 		final int destReg = getRegisterLocation(dest);
 		asm.iload(destReg, var1Reg);
 		operator.handle(destReg, var2Reg, asm);
-/*
-		final BrilVars2.VarLocation location = getLocation(dest);
-		final int destRegister = location.isRegister() ? location.reg() : A_REGISTER;
-		iloadTo(var1, destRegister, asm);
-		final int register2 = iload(var2, B_REGISTER, asm);
-
-		operator.handle(destRegister, register2, asm);
-
-		if (location.isStackOffset()) {
-			asm.istoreToStack(destRegister, FP_REGISTER, location.offset());
-		}
-*/
 	}
 
 	public void b_binaryOperator(String dest, String var1, String var2, BrilAsm asm, B_BinaryRegisterOperator operator) {
@@ -131,14 +106,6 @@ final class BrilVarMapping {
 		final int var2Reg = getRegisterLocation(var2);
 		final int destReg = getRegisterLocation(dest);
 		operator.handle(destReg, var1Reg, var2Reg, asm);
-/*
-		final int register1 = iload(var1, A_REGISTER, asm);
-		final int register2 = iload(var2, B_REGISTER, asm);
-
-		operator.handle(A_REGISTER, register1, register2, asm);
-
-		bstore(dest, A_REGISTER, asm);
-*/
 	}
 
 	public void id(String dest, String type, String src, BrilAsm asm) {
@@ -198,65 +165,9 @@ final class BrilVarMapping {
 	public void branch(String var, String thenLabel, String elseLabel, BrilAsm asm) {
 		final int register = getRegisterLocation(var);
 		asm.brIfElse(register, thenLabel, elseLabel);
-/*
-		final int register = bload(var, A_REGISTER, asm);
-		asm.brIfElse(register, thenLabel, elseLabel);
-*/
 	}
 
 	// Utils ==================================================================
-
-	private void istore(String var, int register, BrilAsm asm) {
-		final BrilVars.VarLocation location = getLocation(var);
-		if (location.isRegister()) {
-			if (register != location.reg()) {
-				asm.iload(location.reg(), register);
-			}
-		}
-		else {
-			asm.istoreToStack(register, FP_REGISTER, location.offset());
-		}
-	}
-
-	private void bstore(String var, int register, BrilAsm asm) {
-		final BrilVars.VarLocation location = getLocation(var);
-		if (location.isRegister()) {
-			if (register != location.reg()) {
-				asm.bload(location.reg(), register);
-			}
-		}
-		else {
-			asm.bstoreToStack(register, FP_REGISTER, location.offset());
-		}
-	}
-
-	private int iload(String var, int registerForSpilledVar, BrilAsm asm) {
-		final BrilVars.VarLocation location = getLocation(var);
-		if (location.isRegister()) {
-			return location.reg();
-		}
-
-		asm.iloadFromStack(registerForSpilledVar, FP_REGISTER, location.offset());
-		return registerForSpilledVar;
-	}
-
-	@SuppressWarnings("SameParameterValue")
-	private int bload(String var, int registerForSpilledVar, BrilAsm asm) {
-		final BrilVars.VarLocation location = getLocation(var);
-		if (location.isRegister()) {
-			return location.reg();
-		}
-
-		asm.bloadFromStack(registerForSpilledVar, FP_REGISTER, location.offset());
-		return registerForSpilledVar;
-	}
-
-	private void iloadTo(String var, int target, BrilAsm asm) {
-		final int register = iload(var, target, asm);
-		if (register != target) {
-			asm.iload(target, register);
-		}
-	}
 
 	private BrilVars.VarLocation getLocation(String var) {
 		return varToLocation.get(var);
@@ -266,20 +177,6 @@ final class BrilVarMapping {
 		final BrilVars.VarLocation location = varToLocation.get(var);
 		Utils.assertTrue(location.isRegister());
 		return location.reg();
-	}
-
-	private static List<String> addVarInfoTypesForArguments(List<BrilNode> arguments, RegisterColoring registerAllocation) {
-		final List<String> argNames = new ArrayList<>();
-		for (int i = 0; i < arguments.size(); i++) {
-			final BrilNode argument = arguments.get(i);
-			final String argName = BrilFactory.getArgName(argument);
-			final String argType = BrilFactory.getArgType(argument);
-
-			argNames.add(argName);
-			registerAllocation.setRegister(argName, i);
-		}
-		registerAllocation.addEdgesBetween(new HashSet<>(argNames));
-		return argNames;
 	}
 
 	private static Set<String> getLocalVars(List<BrilNode> instructions) {
